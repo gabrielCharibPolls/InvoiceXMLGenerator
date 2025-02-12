8713e3165227fdb0d2e05e4fa301fff6c3f32139
Gabriel Charib <110329448+gabrielCharibPolls@users.noreply.github.com>
2025-02-12 11:27:13 +0100

    Set up a functional JavaFX application

---
+++ test/hello/src/main.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    Label label = new Label("Hello, World!");
+        Scene scene = new Scene(label, 300, 200);
+        
+        primaryStage.setTitle("JavaFX Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
